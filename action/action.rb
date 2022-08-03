c4a0f7f0e08dfd94646adde27a931ebf5fb77535
Mo Lawson <mo@molawson.com>
2022-08-03 12:20:04 -0500

    Filter diff files to only Ruby files
    If we don't do this, SyntaxTree will try to format every file,
    regardless of type. Ideally, this matching would live within SyntaxTree
    itself. But until then, we can grab the Ruby file names and extensions
    from linguist and filter files with that info.

--- action/action.rb
+++ action/action.rb
@@ -2,10 +2,73 @@ require "ostruct"
 
 require_relative "./install_gems"
 
+class RubyFileMatcher
+  # From https://github.com/github/linguist/blob/master/lib/linguist/languages.yml
+  FILENAMES = %w[
+    .irbrc
+    .pryrc
+    .simplecov
+    Appraisals
+    Berksfile
+    Brewfile
+    Buildfile
+    Capfile
+    Dangerfile
+    Deliverfile
+    Fastfile
+    Gemfile
+    Guardfile
+    Jarfile
+    Mavenfile
+    Podfile
+    Puppetfile
+    Rakefile
+    Snapfile
+    Steepfile
+    Thorfile
+    Vagrantfile
+    buildfile
+  ].freeze
+
+  EXTENSIONS = %w[
+    rb
+    builder
+    eye
+    fcgi
+    gemspec
+    god
+    jbuilder
+    mspec
+    pluginspec
+    podspec
+    prawn
+    rabl
+    rake
+    rbi
+    rbuild
+    rbw
+    rbx
+    ru
+    ruby
+    spec
+    thor
+    watchr
+  ].freeze
+
+  def self.match?(path)
+    File.fnmatch?("**/#{FILENAMES.join(",")}", path, File::FNM_EXTGLOB) ||
+      File.fnmatch?("**/*.{#{EXTENSIONS.join(",")}}", path, File::FNM_EXTGLOB)
+  end
+end
+
 event = JSON.parse(
   File.read(ENV["GITHUB_EVENT_PATH"]),
   object_class: OpenStruct
 )
 compare_sha = event.pull_request.base.sha
 stree_cmd = ENV['INPUT_SMARTGEMINSTALL'].to_s.downcase == "false" ? "bundle exec stree" : "stree"
-`git diff --name-only #{compare_sha} --diff-filter AM --relative | xargs #{stree_cmd} write`
+
+changed_files = `git diff --name-only #{compare_sha} --diff-filter AM`
+changed_ruby_files = changed_files.each_line(chomp: true).select { |f| RubyFileMatcher.match?(f) }
+
+`#{stree_cmd} write #{changed_ruby_files.join(" ")}`
